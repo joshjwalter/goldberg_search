d95b14fb9e7ccf1f5abcfcb2ef80f495255b6af0
Joshua Walter <jjw368@gmail.com>
2025-07-27 01:02:47 -0400

    Add functionality for profile managment and implemented slightly (adds to data structure but dont do anything with the data structure yet)
    - Enhanced Target struct to include lists for first names, last names, and addresses.
    - Implemented AddParcel trait for adding parcel data to Target.
    - Updated display_parcels function to show numbered results.
    - Modified main function for pagination in API results.
    - Added .gitignore to exclude target directory.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod target;
+use target::{Target, AddParcel};
 
 use reqwest::blocking::Client;
 use serde_json::Value;
 use std:: io;
 use std::io::Write;
+
 
 fn input(text: &str) -> String {
     print!("{}", text);
@@ -14,7 +16,9 @@ fn input(text: &str) -> String {
 }
 
 fn display_parcels(parcel_vec: &Vec<Value>) {
+    let mut number: u32 = 1;
     for x in parcel_vec {
+        println!("({})", number);
         println!("\n-----START-----");
         println!("Mailing Name: {}", x["attributes"]["MAILING_NAME"]);
         println!("Land Use Description: {}", x["attributes"]["LAND_USE_DESCRIPTION"]);
@@ -24,7 +28,8 @@ fn display_parcels(parcel_vec: &Vec<Value>) {
         println!("Appraised Building Value: {}", x["attributes"]["APPRAISED_BUILDING_VALUE"]);
         println!("Appraised Land Value: {}", x["attributes"]["APPRAISED_LAND_VALUE"]);
         println!("Appraised Total Value: {}", x["attributes"]["APPRAISED_TOTAL_VALUE"]);
-        println!("------END------")
+        println!("------END------");
+        number += 1;
     }
 }
 
@@ -56,11 +61,43 @@ fn db_size() -> u32 {
 fn main() {
     println!("Welcome to Goldberg Search");
     let name = input("Input Target Name (Last First): ").to_lowercase();
-    let result_record_count = input("How many max results would you like? (up to 2000)");
-    let request_string: String = format!("https://scgisa.starkcountyohio.gov/arcgis/rest/services/Auditor/StarkCountyParcels_Viewer/MapServer/0/query?f=json&where=LOWER%28OWNER%29%20LIKE%20%27%25{}%25%27&returnGeometry=false&outFields=*&resultRecordCount={}", name, result_record_count);
-    //println!("{}", request_string);
+    // switch to loop that auto displays 5 each page
+    let result_record_count = input("How many max results would you like per page? (up to 2000)");
+    let viewing_pages: bool = true;
+    let mut page: u32 = 0;
     let client = Client::new();
-    let  result: Value = client.get(request_string).send().unwrap().json().unwrap();
-    let result_vec = result["features"].as_array().unwrap();
-    display_parcels(result_vec);
+    let mut target_file = Target {
+        first_names_list: Vec::new(),
+        last_names_list: Vec::new(),
+        address_list: Vec::new(),
+    };
+    while viewing_pages {
+        let request_string: String = format!("https://scgisa.starkcountyohio.gov/arcgis/rest/services/Auditor/StarkCountyParcels_Viewer/MapServer/0/query?f=json&where=LOWER%28OWNER%29%20LIKE%20%27%25{}%25%27&returnGeometry=false&outFields=*&resultRecordCount={}&resultOFFSET={}", name, result_record_count, page);
+        let  result: Value = client.get(request_string).send().unwrap().json().unwrap();
+        let result_vec = result["features"].as_array().unwrap();
+        if result_vec.len() == 0 {
+            println!("No more results, restarting from the beginning");
+            page = 0;
+            continue;
+        } else {
+            display_parcels(result_vec);
+        let input_string = input("Select all choices that seem reasonable (0 for next page) ('end' to end search)");
+        let choice = input_string.trim().split(",").collect::<Vec<&str>>();
+        if choice.clone()[0] == "0" {
+            page += 1;
+            continue;
+        } else if choice.clone()[0] == "end" {
+            println!("Search manually ended");
+            break;
+            // for now just break and end program but in future go back to selection menu or something
+            // main(); allows the program to restart a new search
+            
+        } else { // add an else if for if it is an integer, becuae all edge cases hit errors with just else
+            for x in choice {
+                let x_num: usize = x.parse().unwrap();
+                target_file = target_file.add_parcel(&result_vec[x_num-1]);
+            }
+        }
+        }
+    }
 }
--- src/target.rs
+++ src/target.rs
@@ -1,9 +1,33 @@
 // build a target profile that can be saved to something like a json file (idk prob not a custom file type cuz that would suck) and be able to
 // resume editing and build a case on somebody with customn data gathered without automation too (eventually maybe add webscraping or find ways to automate everyting)
+use serde_json::Value;
 
 pub struct Target {
-    first_name: String,
-    realated_first_names: Vec<String>,
-    last_name: String,
-    address: String,
+    // TODO: add a field that keeps each of the entried as one Vec Each, or maybe just make the Target have identifiers or something because...
+    // just the index might not be enogh
+    pub first_names_list: Vec<String>,
+    pub last_names_list: Vec<String>,
+    pub address_list: Vec<String>,
+}
+
+// TODO: add a trait that saves to heroku sql database or something
+// maybe just json file
+
+pub trait AddParcel {
+    fn add_parcel(self, parcel: &Value) -> Target;
+}
+
+impl AddParcel for Target {
+    fn add_parcel(mut self, parcel: &Value) -> Target {
+        let name: String = parcel["attributes"]["MAILING_NAME"].to_string();
+        let name_seperated: Vec<&str> = name.trim().split(" ").collect();
+
+        // names in the DB are usually Last name, then First name
+        self.first_names_list.push(name_seperated[1].to_string());
+        self.last_names_list.push(name_seperated[0].to_string());
+
+        self.address_list.push(parcel["attributes"]["MAILING_ADDRESS"].to_string());
+
+        return self
+    }
 }
