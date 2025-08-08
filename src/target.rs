ad32ba8a8a670b49bb48f4cece4effc823e25afa
Joshua Walter <jjw368@gmail.com>
2025-08-08 15:30:57 -0400

    Refactor Target struct and remove Expandable Trait

--- src/target.rs
+++ src/target.rs
@@ -2,76 +2,91 @@
 // resume editing and build a case on somebody with customn data gathered without automation too (eventually maybe add webscraping or find ways to automate everyting)
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
 
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Parcel {
+    first_name: String,
+    last_name: String,
+    address: String
+}
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Target {
     // TODO: add a field that keeps each of the entried as one Vec Each, or maybe just make the Target have identifiers or something because...
     // just the index might not be enogh
-    pub first_names_list: Vec<String>,
-    pub last_names_list: Vec<String>,
-    pub address_list: Vec<String>,
+    pub parcel_list: HashMap<String, Parcel>,
 }
 
 // TODO: add a trait that saves to heroku sql database or something
 // maybe just json file
 
-pub trait Expandable {
-    fn add_parcel(self, parcel: &Value) -> Target;
-}
 
-impl Expandable for Target {
-    fn add_parcel(mut self, parcel: &Value) -> Target {
+//change form returning Target to returning () so that it just edits the borrowed self instead of a new Target
+
+impl Target {
+    pub fn new() -> Self {
+        Self {
+            parcel_list: HashMap::new()
+        }
+    }
+    pub fn add_parcel(&mut self, parcel: &Value) {
+        let current_parcel_list_length: usize = self.parcel_list.len();
         let name: String = parcel["attributes"]["MAILING_NAME"].to_string();
         let name_seperated: Vec<&str> = name.trim().split(" ").collect();
 
+        let addr = parcel["attributes"]["MAILING_ADDRESS"]
+            .as_str()
+            .unwrap_or("")
+            .to_string(); // safe extract
+
         // names in the DB are usually Last name, then First name
-        if name_seperated.len() >= 2 {
-            self.first_names_list.push(name_seperated[1].to_string());
-            self.last_names_list.push(name_seperated[0].to_string());
-        } else {
-            self.last_names_list.push(name.to_string());
+        match name_seperated.len() {
+            0 => {
+                self.parcel_list
+                    .insert(
+                        current_parcel_list_length.to_string(),
+                        Parcel {
+                            first_name: String::new(), 
+                            last_name: String::new(), 
+                            address: String::from(addr)
+                        }
+                    );
+            },
+            1 => {
+                self.parcel_list
+                    .insert(
+                        current_parcel_list_length.to_string(),
+                        Parcel {
+                            first_name: String::new(), 
+                            last_name: String::from(name_seperated[0]), 
+                            address: String::from(addr)
+                        }
+                    );
+            },
+            _ => {
+                self.parcel_list
+                    .insert(
+                        current_parcel_list_length.to_string(),
+                        Parcel {
+                            first_name: String::from(name_seperated[1]), 
+                            last_name: String::from(name_seperated[0]), 
+                            address: String::from(addr)
+                        }
+                    );
+            }
         }
-
-
-        self.address_list
-            .push(parcel["attributes"]["MAILING_ADDRESS"].to_string());
-
-        self
     }
-}
-
-pub trait Saveable {
-    /// Saves the struct as a JSON file.
-    fn save_to_json(&self, file_path: &str) -> Result<(), std::io::Error>;
-}
-
-impl Saveable for Target {
-    fn save_to_json(&self, file_path: &str) -> Result<(), std::io::Error> {
+    pub fn save_to_json(&self, file_path: &str) -> Result<(), std::io::Error> {
         let json_string = serde_json::to_string_pretty(self).unwrap();
 
-        let mut file = File::create(Path::new(file_path))?;
+        let mut file = File::create(file_path)?;
 
         file.write_all(json_string.as_bytes())?;
 
         println!("Profile exported to {}", file_path);
         Ok(())
-    }
-}
-
-pub trait Importable {
-    /// Imports JSON file to Target struct
-    fn import_from_json(&self, file_path: &str)-> Target;
-}
-
-impl Importable for Target {
-    fn import_from_json(&self, file_path: &str)-> Target {
-        let file = File::open(Path::new(file_path)).unwrap();
-        
-        let imported_target: Target = serde_json::from_reader(file).unwrap();
-
-        return imported_target;
     }
 }
